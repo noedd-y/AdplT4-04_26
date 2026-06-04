397454d237317ce688bbaf16c92bad6c8ec7f255
noedd-y <Rayzan.wirayuda@gmail.com>
2026-06-04 23:17:13 +0700

    Updated transaction to accomodate different type of command

--- Entity/Transaction.java
+++ Entity/Transaction.java
@@ -1,39 +1,34 @@
 package entity;
+
 public class Transaction {
+    enum TransactionType {
+        BORROW, RETURN, RESERVE, CANCEL_RESERVATION
+    }
     Book book;
     User user;
     String transactionDate;
     String returnDate;
-    public Transaction(Book book, User user, String transactionDate, String returnDate) {
+    String state;
+    TransactionType type;
+    //default constructor, for reserve and cancel reserve transaction
+    //transaction type is in param to explicitly indicate the type of transaction (instead of choosing the constructor based on the transaction type, which is less clear)
+    public Transaction(TransactionType type, Book book, User user, String transactionDate) {
         this.book = book;
         this.user = user;
         this.transactionDate = transactionDate;
-        this.returnDate = returnDate;
+        this.type = type;
+        //not used for reserve and cancel reserve transaction, since they dont have return date and state
+        this.returnDate = null;
+        this.state = null;
     }
-    
-    public Book getBook() {
-        return book;
+
+    //constructor for borrow transaction and return transaction, have additional date
+    public Transaction(TransactionType type, Book book, User user, String transactionDate, String data) {
+        this(type, book, user, transactionDate);
+        if(type == TransactionType.BORROW) {
+            this.returnDate = data;
+        } else if(type == TransactionType.RETURN) {
+            this.state = data;
+        }
     }
-    public void setBook(Book book) {
-        this.book = book;
-    }
-    public User getUser() {
-        return user;
-    }
-    public void setUser(User user) {
-        this.user = user;
-    }
-    public String getTransactionDate() {
-        return transactionDate;
-    }
-    public void setTransactionDate(String transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-    public String getReturnDate() {
-        return returnDate;
-    }
-    public void setReturnDate(String returnDate) {
-        this.returnDate = returnDate;
-    }
-    
 }
